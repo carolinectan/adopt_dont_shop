49b561dfeed6c4fedbdb0f2008b25f45212f80e7
Caroline Tan <carolinectan3@gmail.com>
2021-07-17 19:14:34 -0400

    Remove comments

--- spec/features/applications/create_spec.rb
+++ spec/features/applications/create_spec.rb
@@ -87,14 +87,5 @@ RSpec.describe 'application new page' do
       expect(page).to have_content("Zip Code")
       expect(page).to have_content("Description")
     end
-    # Starting an Application, Form not Completed
-    #
-    # As a visitor
-    # When I visit the new application page
-    # And I fail to fill in any of the form fields
-    # And I click submit
-    # Then I am taken back to the new applications page
-    # And I see a message that I must fill in those fields.
-
   end
 end
