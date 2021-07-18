ef547ae8f2c157929959aae82a1fe625b75ab4fe
Caroline Tan <carolinectan3@gmail.com>
2021-07-18 14:31:23 -0400

    Refactor

--- spec/features/pets/show_spec.rb
+++ spec/features/pets/show_spec.rb
@@ -1,28 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe 'the shelter show' do
+  before :each do
+    @shelter = Shelter.create(name: 'Mystery Building', city: 'Irvine CA', foster_program: false, rank: 9)
+    @pet1 = Pet.create(name: 'Scooby', age: 2, breed: 'Great Dane', adoptable: true, shelter_id: @shelter.id)
+    @pet2 = Pet.create(name: 'Scrappy', age: 1, breed: 'Great Dane', adoptable: true, shelter_id: @shelter.id)
+
+  end
+
   it "shows the shelter and all it's attributes" do
-    shelter = Shelter.create(name: 'Mystery Building', city: 'Irvine CA', foster_program: false, rank: 9)
-    pet = Pet.create(name: 'Scooby', age: 2, breed: 'Great Dane', adoptable: true, shelter_id: shelter.id)
 
-    visit "/pets/#{pet.id}"
+    visit "/pets/#{@pet1.id}"
 
-    expect(page).to have_content(pet.name)
-    expect(page).to have_content(pet.age)
-    expect(page).to have_content(pet.adoptable)
-    expect(page).to have_content(pet.breed)
-    expect(page).to have_content(pet.shelter_name)
+    expect(page).to have_content(@pet1.name)
+    expect(page).to have_content(@pet1.age)
+    expect(page).to have_content(@pet1.adoptable)
+    expect(page).to have_content(@pet1.breed)
+    expect(page).to have_content(@pet1.shelter_name)
   end
 
   it "allows the user to delete a pet" do
-    shelter = Shelter.create(name: 'Mystery Building', city: 'Irvine CA', foster_program: false, rank: 9)
-    pet = Pet.create(name: 'Scrappy', age: 1, breed: 'Great Dane', adoptable: true, shelter_id: shelter.id)
+    visit "/pets/#{@pet2.id}"
 
-    visit "/pets/#{pet.id}"
-
-    click_on("Delete #{pet.name}")
+    click_on("Delete #{@pet2.name}")
 
     expect(page).to have_current_path('/pets')
-    expect(page).to_not have_content(pet.name)
+    expect(page).to_not have_content("#{@pet2.name}")
   end
 end
