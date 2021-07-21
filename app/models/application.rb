2bfe14112d8fdda0cccb8d303029798714e1ff35
Caroline Tan <carolinectan3@gmail.com>
2021-07-20 23:21:43 -0400

    Refactor; add comments

--- app/models/application.rb
+++ app/models/application.rb
@@ -8,4 +8,12 @@ class Application < ApplicationRecord
 
   has_many :pet_applications
   has_many :pets, through: :pet_applications
+
+  def pets_length?
+    pets.length > 0
+  end
 end
+
+
+# specific model -> specific controller for that model -> view / crud for that model
+# single responsibility principle ^^ a model deals with one resource, a controller deals with one model, a view deals wiht one crud action for a specific model
